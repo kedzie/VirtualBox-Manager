c80723f0286a9c43a9a47df23e73d1567bf019f5
kedzie <mark.kedzierski@gmail.com>
2012-09-02 14:20:41 -0500

    updated init script

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo Initializing eclipse projects..
-
+git submodule init
+git submodule update
 cat tree-view-list-android/.project | sed -e s/tree-view-list-android/ActionBarSherlock/ > ActionBarSherlock/library/.project
 cp tree-view-list-android/.classpath ActionBarSherlock/library/.classpath
