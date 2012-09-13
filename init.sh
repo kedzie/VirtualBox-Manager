082ef0cc78e931ac464f887476cd901fa34f3217
Marek Kedzierski <mark.kedzierski@gmail.com>
2012-09-13 16:30:43 -0500

    init.sh checks out branches

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-echo Initializing eclipse projects..
+echo Initializing eclipse projects with branch [$1]..
 git submodule init
 git submodule update
 cat tree-view-list-android/.project | sed -e s/tree-view-list-android/ActionBarSherlock/ > ActionBarSherlock/library/.project
 cp tree-view-list-android/.classpath ActionBarSherlock/library/.classpath
+git submodule foreach git checkout $1
